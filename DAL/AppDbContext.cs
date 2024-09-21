187b2c439d978f982e1b6026c08386ca510c5b3f
xtuna <92073982+xtuna@users.noreply.github.com>
2024-09-21 15:38:19 +0800

    Add files via upload

---
+++ DAL/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace chief.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Application> Applications { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Register the trigger
+            modelBuilder.Entity<Application>()
+                .ToTable(tb => tb.HasTrigger("trg_UpdateApplicationStatus"));
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Pages/ApplicationMaster/Delete.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using chief.DAL;
+
+namespace chief.Pages.ApplicationMaster
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly chief.DAL.AppDbContext _context;
+
+        public DeleteModel(chief.DAL.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Application Application { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var application = await _context.Applications.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (application == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Application = application;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var application = await _context.Applications.FindAsync(id);
+            if (application != null)
+            {
+                Application = application;
+                _context.Applications.Remove(Application);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
