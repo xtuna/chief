731b9c19fffe20bce3d5b2ac6416f6ebbb0ab0f9
xtuna <92073982+xtuna@users.noreply.github.com>
2024-10-02 17:09:18 +0800

    Add files via upload

---
+++ Migrations/20241002085246_intial.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace chief.Migrations
+{
+    /// <inheritdoc />
+    public partial class intial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Applications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DTSNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ApplicationName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ApplicantName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FiledDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Evaluators = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DaysToEvaluate = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Applications", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Checklists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DocumentName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ApplicationType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ChecklistItems = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Checklists", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Notifs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notifs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ApplicationFiles",
+                columns: table => new
+                {
+                    ApplicationId = table.Column<int>(type: "int", nullable: false),
+                    UploadedFileName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationFiles", x => x.ApplicationId);
+                    table.ForeignKey(
+                        name: "FK_ApplicationFiles_Applications_ApplicationId",
+                        column: x => x.ApplicationId,
+                        principalTable: "Applications",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApplicationFiles");
+
+            migrationBuilder.DropTable(
+                name: "Checklists");
+
+            migrationBuilder.DropTable(
+                name: "Notifs");
+
+            migrationBuilder.DropTable(
+                name: "Applications");
+        }
+    }
+}
---
+++ Migrations/20241002090213_AddUploadedFileNameColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace chief.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUploadedFileNameColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UploadedFileName",
+                table: "Applications",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UploadedFileName",
+                table: "Applications");
+        }
+    }
+}
