0b38c74582ebeba39cf305c5a10e5c6a00968588
xtuna <92073982+xtuna@users.noreply.github.com>
2024-10-09 23:52:57 +0800

    Update Edit.cshtml.cs

--- Pages/NotifyMaster/Edit.cshtml.cs
+++ Pages/NotifyMaster/Edit.cshtml.cs
@@ -38,7 +38,6 @@ namespace chief.Pages.NotifyMaster
             return Page();
         }
 
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more information, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
