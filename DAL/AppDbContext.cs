ec9b90e3aaf3820dcd812fd916c59784ce05a88f
xtuna <92073982+xtuna@users.noreply.github.com>
2024-10-09 21:32:23 +0800

    Update AppDbContext.cs

--- DAL/AppDbContext.cs
+++ DAL/AppDbContext.cs
@@ -10,14 +10,17 @@ namespace chief.DAL
         public DbSet<Application> Applications { get; set; }
         public DbSet<ApplicationFile> ApplicationFiles { get; set; }
         public DbSet<Checklist> Checklists { get; set; }
+        public DbSet<ChecklistItem> ChecklistItems { get; set; }
         public DbSet<Notify> Notifs { get; set; }
+        public DbSet<Evaluator> Evaluators { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Application>()
-                .ToTable(tb => tb.HasTrigger("trg_UpdateApplicationStatus"));
-
-            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Checklist>()
+                .HasMany(c => c.ChecklistItems)
+                .WithOne(c => c.Checklist)
+                .HasForeignKey(c => c.ChecklistId)
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
